632515258b4358e9e9a7396f917d77f587854ca7
Lynnmich <lynnenmichuki@gmail.com>
2023-08-31 21:30:44 +0300

    Task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,13 @@ def hash_password(password: str) -> bytes:
     """function that takes a str argument and encodes it and returns a
     hashed password which is a byte string"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """this function checks wether provided password matches the
+    hashed password
+    """
+    is_valid = False
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        is_valid = True
+    return is_valid
