ca868147f02c74b7b549ef4d88eb9a283ec889f5
Lynnmich <lynnenmichuki@gmail.com>
2023-08-31 18:53:49 +0300

    Task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""A function called filter_datum that returns the log message obfuscated"""
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator:str) -> str:
+    """Filter data function"""
+    for field in fields:
+        message = re.sub(rf"{field}=(.*?)\{separator}",
+                         f'{field}={redaction}{separator}', message)
+    return message
