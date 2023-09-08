e53b74e1cf756a10f7de7528a4e64677e54b3fe2
Lynnmich <lynnenmichuki@gmail.com>
2023-09-08 15:04:03 +0300

    Task 2

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,39 +1,39 @@
 #!/usr/bin/env python3
-"""Auth class"""
-
-from tabnanny import check
+""" Authorization module
+"""
+import re
+from typing import List, TypeVar
 from flask import request
-from typing import TypeVar, List
-from os import getenv
-User = TypeVar('User')
 
 
-class Auth:
-    """A class to manage the API authentication"""
-
+class Auth():
+    """ Authorization Class
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """returns False - path and excluded_paths"""
-        check = path
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        """ Method to define which routes don't need authentication
+        """
+        if path is None or excluded_paths is None:
             return True
-        if path[-1] != "/":
-            check += "/"
-        if check in excluded_paths or path in excluded_paths:
-            return False
+        for st in excluded_paths:
+            if path == st + '/' or path + '/' == st or path == st:
+                return False
+            pattern = re.compile(st)
+            res = pattern.match(path)
+            if res:
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """returns None - request"""
+        """ Method to validate all requests to secure the API
+        """
         if request is None:
             return None
-        return request.headers.get("Authorization")
+        try:
+            st = request.headers['Authorization']
+        except KeyError:
+            st = None
+        return st
 
-    def current_user(self, request=None) -> User:
-        """returns None - request"""
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Temporarily Return None """
         return None
-
-    def session_cookie(self, request=None):
-        """returns a cookie from a request"""
-        if request:
-            session_name = getenv("SESSION_NAME")
-            return request.cookie.get(session_name, None)
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,34 +1,48 @@
 #!/usr/bin/env python3
-"""SessionAuth"""
-
+""" Session Authorization module
+"""
+import uuid
 from api.v1.auth.auth import Auth
-from typing import TypeVar
-from uuid import uuid4
 from models.user import User
 
 
 class SessionAuth(Auth):
-    """Session auth class"""
+    """ Basic Authorization Class
+    Inheriting from Auth
+    """
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """creates a Session ID for a user_id"""
-        if not isinstance(user_id, str):
-            return
-        session_id = str(uuid4())
-        SessionAuth.user_id_by_session_id[user_id] = session_id
-        return session_id
+        """ instance method to create a session
+        """
+        if not user_id or type(user_id) is not str:
+            return None
+        sess_id = str(uuid.uuid4())
+        self.user_id_by_session_id[sess_id] = user_id
+        return sess_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """returns a User ID based on a Session ID"""
-        if not isinstance(session_id, str):
-            return
-        return SessionAuth.user_id_by_session_id.get(session_id, None)
+        """ method to get User id based on session id
+        """
+        if not session_id or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """current_user"""
-        if request:
-            session_cookie = self.session_cookie(request)
-            if session_cookie:
-                user_id = self.user_id_for_session_id(session_cookie)
-                return User.get(user_id)
+    def current_user(self, request=None):
+        """ method to get current user based on cookies
+        """
+        sess_id = self.session_cookie(request)
+        u_id = self.user_id_for_session_id(sess_id)
+        return User.get(u_id)
+
+    def destroy_session(self, request=None):
+        """ method to destroy session
+        """
+        sess_id = self.session_cookie(request)
+        if not request or not sess_id:
+            return False
+        u_id = self.user_id_for_session_id(sess_id)
+        if not u_id:
+            return False
+        self.user_id_by_session_id.pop(sess_id)
+        return True
